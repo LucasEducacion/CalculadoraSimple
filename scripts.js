786ce559e1125c2d39584a4ef7e7a63347048f27
LucasEducacion <llucero168@alumno.unlam.edu.ar>
2025-05-15 21:35:18 -0300

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,27 @@
+const input = document.getElementById('resultado');
+const numeros = document.querySelectorAll('.btN');
+const historial = document.getElementById('historial');
+
+numeros.forEach(numero => {
+    numero.addEventListener('click', () => {
+        input.value += numero.textContent;
+    });
+});
+
+function borrarNumeros() {
+    input.value = '';
+}
+
+function realizarOperacion() {
+    try {
+        const expresion = input.value;
+        const resultado = eval(expresion);
+        let historia = document.createElement('li');
+        historia.innerHTML = resultado;
+        historial.appendChild(historia);
+        alert(`Resultado: ${resultado}`);
+    } catch (error) {
+        alert('Expresión inválida');
+    }
+    input.value = '';
+}
